1470b1bc3e7259e43415fe67d6a530b4245fe30d
mkamaUO <mkama022@uottawa.ca>
2025-10-27 23:02:37 -0400

    Update mux41.vhd

--- mux41.vhd
+++ mux41.vhd
@@ -11,9 +11,38 @@ END mux41;
 
 ARCHITECTURE rtl OF mux41 IS
   SIGNAL int_output           : STD_LOGIC;
+  SIGNAL mux_output           : STD_LOGIC_VECTOR(1 DOWNTO 0);
+  COMPONENT mux21
+    PORT (
+      i_A, i_B, i_s : in STD_LOGIC; 
+      o : out STD_LOGIC
+    );
+  END COMPONENT;
 BEGIN
   -- Concurrent Signals
-  int_output <= ((not i_s0) and (not i_s1) and i_A) or (i_s0 and (not i_s1) and i_B) or ((not i_s0) and i_s1 and i_C) or (i_s0 and i_s1 and i_D);
+  mux1: mux21
+		PORT MAP(
+      i_A => i_A, 
+      i_B => i_B,
+      i_s => i_s1, 
+      o => mux_output(1)
+    );
+		
+	mux2: mux21
+		PORT MAP(
+      i_A => i_C, 
+      i_B => i_D,
+      i_s => i_s1, 
+      o => mux_output(0)
+    );
+		
+	mux3: mux21
+		PORT MAP(
+      i_A => mux_output(1), 
+      i_B => mux_output(0),
+      i_s => i_s0, 
+      o => int_output
+    );
   -- Output Driver;
   o <= int_output;
 END rtl;
